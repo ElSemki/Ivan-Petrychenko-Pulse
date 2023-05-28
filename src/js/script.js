fbc09f4101ba376dc287a1902e11404393223dc3
Alexandr Alexeev <alexandr-alexeev5452@rambler.ru>
2023-05-28 21:41:48 +0300

    catalog + adaptive + js

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,38 @@ document
 document
   .querySelector('.carousel__next')
   .addEventListener('click', () => slider.goTo('next'));
+
+$(document).ready(function () {
+  $('ul.catalog__tabs').on(
+    'click',
+    'li:not(.catalog__tab--active)',
+    function () {
+      $(this)
+        .addClass('catalog__tab--active')
+        .siblings()
+        .removeClass('catalog__tab--active')
+        .closest('div.container')
+        .find('div.catalog__content')
+        .removeClass('catalog__content--active')
+        .eq($(this).index())
+        .addClass('catalog__content--active');
+    }
+  );
+
+  function toggleSlide(className) {
+    $(className).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content')
+          .eq(i)
+          .toggleClass('catalog-item__content--active');
+        $('.catalog-item__list')
+          .eq(i)
+          .toggleClass('catalog-item__list--active');
+      });
+    });
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+});
